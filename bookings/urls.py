368860a7614ddf65f522b9f3e7262b5f417cff5d
Didiana001 <musondakalobwe0@gmail.com>
2025-07-23 18:12:30 +0200

    basic functions working

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import home, booking, success
 
 urlpatterns = [
     path('', home, name='home'),
     path('booking/', booking, name='booking'),
     path('success/', success, name='success'),
 ]
-# Compare this snippet from polished_nailbar/bookings/models.py:
+# Compare this snippet from polished_nailbar/bookings/models.py: 
--- polished_nailbar/settings.py
+++ polished_nailbar/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -38,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookings',
-    'media/'
+    
 ]
 
 MIDDLEWARE = [
@@ -56,7 +62,7 @@ ROOT_URLCONF = 'polished_nailbar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
